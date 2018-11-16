c7cf763c520ebecb060db915afeeb620cc614288
Miguel Cabrera <syntacticsolutions@gmail.com>
2018-11-16 08:40:26 -0800

    initial commit

---
+++ src/VCountryDropdown.vue
@@ -0,0 +1,38 @@
+<template>
+    <Select
+        @change="$emit('change', $event)"
+        @input="$emit('input', $event)"
+        :placeholder="placeholder"
+        :value="value">
+        <Option v-for="(name, slug) in countries" :key="slug" :value="name[1]">{{ name[0] }}</Option>
+    </Select>
+</template>
+
+<script>
+
+import CountryRegionData from '../fixtures/countries.js'
+
+export default {
+    props: ['whitelist', 'blacklist', 'placeholder', 'value'],
+    data: () => ({
+        countries: []
+    }),
+    methods: {
+        filterCountries (countries, whitelist, blacklist) {
+            let filteredCountries = countries
+
+            if (whitelist && whitelist.length > 0) {
+                filteredCountries = countries.filter(([, countrySlug]) => { return whitelist.indexOf(countrySlug) > -1 })
+            }
+            else if (blacklist && blacklist.length > 0) {
+                filteredCountries = countries.filter(([, countrySlug]) => { return blacklist.indexOf(countrySlug) === -1 })
+            }
+
+            return filteredCountries
+        }
+    },
+    mounted () {
+        this.countries = this.filterCountries(CountryRegionData, this.$props.whitelist, this.$props.blacklist)
+    }
+}
+</script>
---
+++ src/VRegionDropdown.vue
@@ -0,0 +1,50 @@
+<template>
+    <Select
+        @change="$emit('change', $event)"
+        @input="$emit('input', $event)"
+        :placeholder="placeholder"
+        :value="value">
+        <Option v-for="(obj, slug) in regions" :key="slug" :value="obj.regionShortCode">{{ obj.regionName }}</Option>
+    </Select>
+</template>
+
+<script>
+import CountryRegionData from '../fixtures/countries.js'
+
+const C = {
+  DISPLAY_TYPE_FULL: 'full',
+  DISPLAY_TYPE_SHORT: 'short',
+  REGION_LIST_DELIMITER: '|',
+  SINGLE_REGION_DELIMITER: '~'
+}
+
+export default {
+    props: ['country', 'placeholder', 'value'],
+    computed: {
+        regions () {
+            if (!this.country) {
+                return []
+            }
+            let regions = []
+
+            CountryRegionData.forEach((i) => {
+                if (i[1] === this.country) {
+                    regions = i
+                }
+            })
+
+            if (!regions || regions.length === 1) {
+                console.error('Error. Unknown country passed: ' + this.country + '. If you\'re passing a country shortcode, be sure to include countryValueType="short" on the RegionDropdown')
+                return []
+            }
+            return regions[2]
+                .split(C.REGION_LIST_DELIMITER)
+                .map((regionPair) => {
+                    let [regionName, regionShortCode = null] = regionPair.split(C.SINGLE_REGION_DELIMITER)
+                    return { regionName, regionShortCode }
+                })
+        }
+    }
+}
+</script>
+
